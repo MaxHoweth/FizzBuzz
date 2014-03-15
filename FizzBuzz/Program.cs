6fee063bf137262f08f560435d771d33fc7fcced
Max <rebelion5418@gmail.com>
2014-03-15 10:12:07 -0700

    FizzBuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			for(int x = 0;x < 100;x++) {
+				
+				if(x % 5 != 0 && x % 3 != 0) {
+					Console.WriteLine(x);
+				}
+				
+				if(x % 5 == 0 && x != 0) {
+					Console.WriteLine("Buzz");	
+				}
+				
+				if(x % 3 == 0 && x != 0) {
+					Console.WriteLine("Fizz");	
+				}
+		
+			}
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
